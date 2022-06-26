fce6d004801023325c060effc6b538746c784bac
AegisVP <vlad@pysarenko.com>
2022-06-27 00:03:25 +0200

    part1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
 
+startBtnRef.addEventListener('click', onStartClick);
+stopBtnRef.addEventListener('click', onStopClick);
+
+let timerColorSwitcher = null;
+let originalBgColor = null;
+// let isTimerStarted = false;
+
+function onStartClick() {
+  // if (isTimerStarted) return;
+  originalBgColor = document.body.style.backgroundColor;
+
+  timerColorSwitcher = setInterval(doChangeBgColor, 1000);
+
+  // isTimerStarted = true;
+  setButtonState(startBtnRef, true);
+}
+
+function onStopClick() {
+  clearInterval(timerColorSwitcher);
+
+  document.body.style.backgroundColor = originalBgColor;
+
+  // isTimerStarted = false;
+  setButtonState(startBtnRef, false);
+}
+
+function doChangeBgColor(newBgColor = getRandomHexColor()) {
+  document.body.style.backgroundColor = newBgColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setButtonState(btn, state = true) {
+  if (state) btn?.setAttribute('disabled', true);
+  else btn?.removeAttribute('disabled');
+
+  console.log(
+    `Set button "${btn.textContent}" state to ${
+      !state ? 'enabled' : 'disabled'
+    }`
+  );
+}
